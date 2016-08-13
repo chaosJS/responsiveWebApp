705bda4e26f0678823d006107e21234d7b82bf5d
chaos_JS <lichao779@sina.com>
2016-08-13 16:10:29 +0800

    广告图片轮播，不同分辨率下图片兼容

---
+++ README.md
@@ -0,0 +1,4 @@
+1. 响应式图片polyfill：
+	1. picturefill库，引入后，可以使用`<picture></picture>`和`<source></source>`标签
+
+2. png压缩
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,15 @@
 /**
  * Created by Administrator on 2016/8/12 0012.
  */
+/*图片轮播js*/
+$(document).ready(
+    function(){
+        $('.owl-carousel').owlCarousel({
+            items:1,//
+            loops:true,
+            autoplay:true,
+            autoplayTimeout:3000,
+            autoplayHoverPause:true,
+            });
+    }
+);
